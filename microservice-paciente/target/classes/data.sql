aab5c101f2ef780b24ab73208e04a72e34c3f383
Nilan-Lukas-Torrealba-Jara <nilantorrealba2002@gmail.com>
2025-07-11 09:48:15 -0400

    Delete microservice-paciente directory

--- microservice-paciente/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO paciente (nombre, apellido, rut, fecha_nacimiento, correo, telefono, direccion)
-VALUES
-('Juan', 'Pérez', '12345678-9', '1990-05-15', 'juan.perez@example.com', '+56912345678', 'Calle Falsa 123'),
-('María', 'Gómez', '98765432-1', '1985-11-23', 'maria.gomez@example.com', '+56987654321', 'Av. Siempre Viva 742');
